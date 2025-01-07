d8f3db3ab9e37645cb76689ab142124251e43843
Panthulumahitha <panthulumahitha0620@gmail.com>
2025-01-07 23:06:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+// Optional JavaScript functionality
+// You can add interactivity here, such as playing specific songs or videos dynamically.
+
+console.log("Welcome to My Playlist!");
